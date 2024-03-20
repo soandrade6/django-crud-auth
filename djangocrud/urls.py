d9e9848ba9b465dbbf6bfec9759c6754b5e8cbcc
soandrade6 <sofiapl2018@gmail.com>
2024-03-19 22:20:13 -0500

    signup update

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -20,6 +20,6 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.helloworld),
-    path('signup/', views.helloworld)
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 
-def helloworld(request):
-    return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html' , {
+            'form': UserCreationForm})
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return HttpResponse('Username already exists')
+        return HttpResponse('Password do not match')
+    
 
 
