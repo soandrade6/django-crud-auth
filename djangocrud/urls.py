af29331dfdbf2dc98420fc19facec8e8962be167
soandrade6 <sofiapl2018@gmail.com>
2024-03-20 21:05:28 -0500

    Login and tasks view

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -21,5 +21,6 @@ from tasks import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('tasks/', views.tasks, name='tasks')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 
 def home(request):
@@ -17,8 +18,9 @@ def signup(request):
             try:
                 user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('User created successfully')
-            except:
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
                 return render(request, 'signup.html', {
                 'form': UserCreationForm,
                 'error': 'Username already exists'
@@ -28,5 +30,6 @@ def signup(request):
                 'error': 'Password do not match'
                 })
     
-
+def tasks(request):
+    return render(request, 'tasks.html')
 
