4986e07a900d8fdec9aea697975a5931d8be3822
soandrade6 <sofiapl2018@gmail.com>
2024-03-20 21:40:27 -0500

    Logout

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.db import IntegrityError
 
 
@@ -33,3 +33,8 @@ def signup(request):
 def tasks(request):
     return render(request, 'tasks.html')
 
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+    
